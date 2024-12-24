d2fb67ab2076f8b0ae8624e7483f15b33781d27a
cyhssin <hssinofficial@gmail.com>
2024-12-24 13:20:02 +0330

    Create User Registration and JWT Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        ser_data = self.serializer_class(data=request.data)
+        if ser_data.is_valid():
+            user = ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
