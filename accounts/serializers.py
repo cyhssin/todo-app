331508477c8400e84e3342afdbdb77ca56336489
cyhssin <hssinofficial@gmail.com>
2024-12-24 13:25:50 +0330

    Adding serializers and urls token authentication

---
+++ accounts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "confirm_password"]
+
+    def create(self, validated_data):
+        if validated_data["password"] != validated_data["confirm_password"]:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        del validated_data["confirm_password"]
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", views.UserRegistrationView.as_view(), name="user_register"),
+]
